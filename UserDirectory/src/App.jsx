4bbce0a19806105af6736c4dcf83b904dfe584d2
Rayala07 <rayala07viswanath@Gmail.com>
2026-01-16 20:52:56 +0530

    Built the Routers

--- UserDirectory/src/App.jsx
+++ UserDirectory/src/App.jsx
@@ -1,6 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Users from "./Pages/Users";
+import UserDetails from "./Pages/UserDetails";
+
 const App = () => {
   return (
-    <div>Hey..</div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/users" element={<Users />} />
+      <Route path="/details" element={<UserDetails />} />
+    </Routes>
   )
 }
 
